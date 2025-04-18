9a447393635c88d2ee1644d4822aab18c99ce112
Sahibzada Issar Ali <issaralisia@gmail.com>
2025-04-18 15:48:18 +0500

    warminig modal

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ let cancel = document.getElementById("cancel");
 cancel.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+// ok model
+let ok = document.getElementById("ok");
+ok.addEventListener("click", function () {
+  document.getElementById("backdrop").classList.add("hidden");
+  document.getElementById("warning-modal").classList.add("hidden");
+  document.body.classList.remove("overflow-hidden");
+});
